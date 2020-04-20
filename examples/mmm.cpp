39e515507bb610d392d6ae9fc43ecbef0b6c5623
Robert Kalescky <rkalescky@smu.edu>
2020-04-20 14:28:32 -0500

    GCC additions.

--- examples/mmm.cpp
+++ examples/mmm.cpp
@@ -2,6 +2,23 @@
 #include <chrono>
 #include <armadillo>
 
+void mmm(arma::mat &A, arma::mat &B, arma::mat &C,
+          const unsigned long int m,
+          const unsigned long int n,
+          const unsigned long int p) {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    for (unsigned long int k = 0; k < p; ++k) { 
+        for (unsigned long int j = 0; j < n; ++j) {
+            for (unsigned long int i = 0; i < m; ++i) {
+                C.at(i, k) += A.at(i, j) * B.at(j, k);
+            }
+        }
+    }
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count()
+              << " ms" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 4) {
         std::cerr << "Usage: " << argv[0] << " i j k" << std::endl;
@@ -16,17 +33,7 @@ int main(int argc, char* argv[]) {
     arma::mat C(m, p, arma::fill::zeros);
     arma::mat D(m, p, arma::fill::zeros);
     D = A*B;
-    auto t1 = std::chrono::high_resolution_clock::now();
-    for (unsigned long int k = 0; k < p; ++k) { 
-        for (unsigned long int j = 0; j < n; ++j) {
-            for (unsigned long int i = 0; i < m; ++i) {
-                C.at(i, k) += A.at(i, j) * B.at(j, k);
-            }
-        }
-    }
-    auto t2 = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count()
-              << " ms" << std::endl;
+    mmm(A, B, C, m, n, p);
     if (arma::approx_equal(C, D, "absdiff", 1E-8)) {
         std::cout << "Success!" << std::endl;
     } else {
